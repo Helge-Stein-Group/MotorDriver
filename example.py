6e9dad73ddbc861af25a06ced549250057272d2d
Danika Heaney <danika.heaney@gmail.com>
2024-02-23 14:56:02 +0100

    updated example

--- example.py
+++ example.py
@@ -2,10 +2,14 @@ import serial
 import time
 from motor_driver import Motor
 
-motor = Motor('COM11', 9600, timeout=3) #add your specific com port here
+motor = Motor('0','COM4', 9600, timeout=3) #add your specific com port here
 print('The motor is connected: ', motor.checkConnection())
+motor.setSpeed(200)
+time.sleep(2)
 motor.move(100)
-motor.setAcceleration(10)
-motor.setSpeed(100)
-motor.move(100)
+time.sleep(2)
+motor.moveUp(400)
+time.sleep(2)
+motor.moveDown(400)
+time.sleep(2)
 
